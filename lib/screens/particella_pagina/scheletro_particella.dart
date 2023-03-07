72d0edb0499fc53635f48ac104f747c78ff84735
noceg43 <noceg43@gmail.com>
2023-03-07 18:32:33 +0100

    mostrare origine dati

--- lib/screens/particella_pagina/scheletro_particella.dart
+++ lib/screens/particella_pagina/scheletro_particella.dart
@@ -1,7 +1,9 @@
 import 'package:demo_1/utils/calcolo_tipo_maggiore.dart';
 import 'package:demo_1/widgets/particella_pagina/grafo_particella.dart';
 import 'package:demo_1/widgets/particella_pagina/scheda_info.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScheletroParticella extends StatelessWidget {
   const ScheletroParticella({super.key, required this.p, required this.s});
@@ -47,6 +49,82 @@ class ScheletroParticella extends StatelessWidget {
                     Grafico(
                       p: p,
                       listVal: snapshot.data!,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      child: Text.rich(
+                        TextSpan(
+                            style: Theme.of(context).textTheme.subtitle2,
+                            children: [
+                              const TextSpan(text: "Dati ottenuti da: "),
+                              (p.tipo == "Inquinamento")
+                                  ? TextSpan(
+                                      children: [
+                                        TextSpan(
+                                            style: const TextStyle(
+                                                color: Colors.blue,
+                                                decoration:
+                                                    TextDecoration.underline),
+                                            //make link blue and underline
+                                            text: "Open-Meteo.com",
+                                            recognizer: TapGestureRecognizer()
+                                              ..onTap = () async {
+                                                //on tap code here, you can navigate to other page or URL
+                                                var urllaunchable =
+                                                    await canLaunchUrl(Uri.parse(
+                                                        "https://open-meteo.com/")); //canLaunch is from url_launcher package
+                                                if (urllaunchable) {
+                                                  await launchUrl(Uri.parse(
+                                                      "https://open-meteo.com/")); //launch is from url_launcher package to launch URL
+                                                } else {
+                                                  //print("URL can't be launched.");
+                                                }
+                                              }),
+                                        const TextSpan(text: " & "),
+                                        TextSpan(
+                                            style: const TextStyle(
+                                                color: Colors.blue,
+                                                decoration:
+                                                    TextDecoration.underline),
+                                            //make link blue and underline
+                                            text: "C.A.M.S.",
+                                            recognizer: TapGestureRecognizer()
+                                              ..onTap = () async {
+                                                //on tap code here, you can navigate to other page or URL
+                                                var urllaunchable =
+                                                    await canLaunchUrl(Uri.parse(
+                                                        "https://atmosphere.copernicus.eu/")); //canLaunch is from url_launcher package
+                                                if (urllaunchable) {
+                                                  await launchUrl(Uri.parse(
+                                                      "https://atmosphere.copernicus.eu/")); //launch is from url_launcher package to launch URL
+                                                } else {
+                                                  //print("URL can't be launched.");
+                                                }
+                                              }),
+                                      ],
+                                    )
+                                  : TextSpan(
+                                      style: const TextStyle(
+                                          color: Colors.blue,
+                                          decoration: TextDecoration.underline),
+                                      //make link blue and underline
+                                      text: "POLLnet.it",
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () async {
+                                          //on tap code here, you can navigate to other page or URL
+                                          var urllaunchable =
+                                              await canLaunchUrl(Uri.parse(
+                                                  "https://www.isprambiente.gov.it/it/banche-dati/banche-dati-folder/aria/aerobiologia")); //canLaunch is from url_launcher package
+                                          if (urllaunchable) {
+                                            await launchUrl(Uri.parse(
+                                                "https://www.isprambiente.gov.it/it/banche-dati/banche-dati-folder/aria/aerobiologia")); //launch is from url_launcher package to launch URL
+                                          } else {
+                                            //print("URL can't be launched.");
+                                          }
+                                        })
+                            ]),
+                        textAlign: TextAlign.end,
+                      ),
                     ),
                     const SizedBox(
                       height: 60,
